4f6c618b1d005781d4202fdebfd3b943714dc6b6
CunhazadaNoDale <cunha12324@gmail.com>
2026-09-19 00:03:30 -0300

    feat: inicio dos metodos voltados para as localizacoes

---
+++ backend-golang/internal/adapter/ibge/location.go
@@ -0,0 +1,29 @@
+package ibge
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/CunhazadanoDale/trads-market-test/internal/adapter/http/dtos"
+)
+
+func (c *Client) GetStates(ctx context.Context) ([]dtos.StateRecord, error) {
+	var response []dtos.StateRecord
+	err := c.Get(ctx, "/localidades/estados", nil, &response)
+	if err != nil {
+		return nil, err
+	}
+	return response, nil
+}
+
+func (c *Client) GetCitiesByState(ctx context.Context, stateUF string) ([]dtos.CityRecord, error) {
+	var response []dtos.CityRecord
+	err := c.GetFromBase(ctx, "/localidades/estados/"+stateUF+"/municipios", nil, &response)
+	if err != nil {
+		return nil, err
+	}
+
+	return response, nil
+}
+
+var _ = http.MethodGet
