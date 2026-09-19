5aa2e32618d5534cce92d867c04cce82e5c6be38
CunhazadaNoDale <cunha12324@gmail.com>
2026-09-18 23:22:17 -0300

    feat: novos data transfers adquiridos

--- backend-golang/internal/adapter/http/dtos/ibge_dto.go
+++ backend-golang/internal/adapter/http/dtos/ibge_dto.go
@@ -1,11 +1,26 @@
 package dtos
 
-
 type Aggregate struct {
-	ID string `json:"id"`
+	ID   string `json:"id"`
 	Nome string `json:"nome"`
 }
 
 type Period struct {
 	Periodo string `json:"periodo"`
+}
+
+type PopulationRecord struct {
+	Localidade Localidade        `json:"localidade"`
+	Serie      map[string]string `json:"serie"`
+}
+
+type Localidade struct {
+	ID    string `json:"id"`
+	Nivel Nivel  `json:"nivel"`
+	Nome  string `json:"nome"`
+}
+
+type Nivel struct {
+	ID   string `json:"id"`
+	Nome string `json:"nome"`
 }
