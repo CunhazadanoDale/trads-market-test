b2cb73f249c51818492cdd726faf09959a32e4c8
CunhazadaNoDale <cunha12324@gmail.com>
2026-09-20 17:20:33 -0300

    feat: complementando com mais codigo o teste do client ibge

--- backend-golang/internal/adapter/ibge/ibge_client_test.go
+++ backend-golang/internal/adapter/ibge/ibge_client_test.go
@@ -33,3 +33,47 @@ func TestPopulationRecordPopulation(t *testing.T) {
 		)
 	}
 }
+
+
+
+func TestPopulationRecordPopulationYearNotFound(t *testing.T) {
+	record := dtos.PopulationRecord{
+		Localidade: dtos.Localidade{
+			ID: "4314506",
+			Nivel: dtos.Nivel{
+				ID:   "N6",
+				Nome: "Município",
+			},
+			Nome: "Pinheiro Machado - RS",
+		},
+		Serie: map[string]string{
+			"2022": "11214",
+		},
+	}
+
+	_, err := PopulationByYear(record, "2021")
+	if err == nil {
+		t.Fatal("expected error when population year is not found")
+	}
+}
+
+func TestPopulationRecordPopulationInvalidValue(t *testing.T) {
+	record := dtos.PopulationRecord{
+		Localidade: dtos.Localidade{
+			ID: "4314506",
+			Nivel: dtos.Nivel{
+				ID:   "N6",
+				Nome: "Município",
+			},
+			Nome: "Pinheiro Machado - RS",
+		},
+		Serie: map[string]string{
+			"2022": "invalid",
+		},
+	}
+
+	_, err := PopulationByYear(record, "2022")
+	if err == nil {
+		t.Fatal("expected error when population value is invalid")
+	}
+}
