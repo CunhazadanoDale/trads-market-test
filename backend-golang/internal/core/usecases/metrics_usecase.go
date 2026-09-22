bac337518b8ae2d71bc2e2022c98865c9f6b30ea
CunhazadaNoDale <cunha12324@gmail.com>
2026-09-22 19:19:53 -0300

    feat: pass-through dos metodos para o repo

---
+++ backend-golang/internal/core/usecases/metrics_usecase.go
@@ -0,0 +1,46 @@
+package usecases
+
+import (
+	"context"
+
+	"github.com/CunhazadanoDale/trads-market-test/internal/core/domain"
+	"github.com/CunhazadanoDale/trads-market-test/internal/core/ports/in"
+	"github.com/CunhazadanoDale/trads-market-test/internal/core/ports/out"
+)
+
+var _ in.MetricsUseCase = (*MetricsUseCaseImpl)(nil)
+
+type MetricsUseCaseImpl struct {
+	repo out.MetricsRepository
+}
+
+func NewMetricsUseCaseImpl(repo out.MetricsRepository) *MetricsUseCaseImpl {
+	return &MetricsUseCaseImpl{
+		repo: repo,
+	}
+}
+
+func (m *MetricsUseCaseImpl) FindNational(
+	ctx context.Context,
+) (domain.NationalMetrics, error) {
+	return m.repo.FindNational(ctx)
+}
+
+func (m *MetricsUseCaseImpl) FindStates(
+	ctx context.Context,
+) ([]domain.StateMetrics, error) {
+	return m.repo.FindStates(ctx)
+}
+
+func (m *MetricsUseCaseImpl) FindTopCities(
+	ctx context.Context,
+	limit int,
+) (domain.TopCities, error) {
+	return m.repo.FindTopCities(ctx, limit)
+}
+
+func (m *MetricsUseCaseImpl) FindAgeDistribution(
+	ctx context.Context,
+) (domain.AgeDistribution, error) {
+	return m.repo.FindAgeDistribution(ctx)
+}
