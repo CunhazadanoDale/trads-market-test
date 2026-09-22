599962cfe61f05460e58ba68d66334aadc192cd9
CunhazadaNoDale <cunha12324@gmail.com>
2026-09-21 21:38:36 -0300

    fix: manteve domain sem tag json

--- backend-golang/internal/core/domain/state.go
+++ backend-golang/internal/core/domain/state.go
@@ -1,9 +1,9 @@
 package domain
 
 type State struct {
-	ID    int64 `db:"id"`
-	IBGECode int64 `db:"ibge_code"`
-	Name string `db:"name"`
-	UF string `db:"uf"`
-	Region string `db:"region"`
+	ID       int64  `db:"id"`
+	IBGECode int64  `db:"ibge_code"`
+	Name     string `db:"name"`
+	UF       string `db:"uf"`
+	Region   string `db:"region"`
 }
